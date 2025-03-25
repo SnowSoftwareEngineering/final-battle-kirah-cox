a0c1216c9dcf15903a33e989c31b628c30aba218
kirah-cox <kirahanncox@gmail.com>
2025-03-24 21:12:50 -0600

    Completed the starter code assignment

--- RPG Battler/Character/Hero.cs
+++ RPG Battler/Character/Hero.cs
@@ -21,11 +21,63 @@ namespace RPG_Battler.Character
         public List<Equipment> Equipment { get; set; }
 
         public Hero()
-        {       
+        {
+            Name = "Unknown";
+            Health = 1;
+            Power = 1;
+            Luck = 1;
+            Mana = 1;
+            Level = 0;
+        }
+
+        public void DisplayStats(bool totalStats = true)
+        {
+            
         }
 
         public void LevelUp()
         {
+            switch (CombatClass)
+            {
+                case CombatClass.None:
+                    break;
+                case CombatClass.Warrior:
+                    Random randomHealth = new Random();
+                    Health += randomHealth.Next(1, 16);
+
+                    Random randomPower = new Random();
+                    Power += randomPower.Next(1, 4);
+
+                    Random randomLuck = new Random();
+                    Luck += randomLuck.Next(3, 6);
+
+                    break;
+                case CombatClass.Wizard:
+                    randomHealth = new Random();
+                    Health += randomHealth.Next(10, 21);
+
+                    randomPower = new Random();
+                    Power += randomPower.Next(1, 4);
+
+                    randomLuck = new Random();
+                    Luck += randomLuck.Next(1, 4);
+
+                    break;
+                case CombatClass.Rogue:
+                    randomHealth = new Random();
+                    Health += randomHealth.Next(1, 16);
+
+                    randomPower = new Random();
+                    Power += randomPower.Next(1, 4);
+
+                    randomLuck = new Random();
+                    Luck += randomLuck.Next(3, 6);
+
+                    break;
+                default:
+                    break;
+            }
+                
         }
 
         public void CalculateTotals()
